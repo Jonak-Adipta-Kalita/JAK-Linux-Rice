00563c289f6a0b7ba1fcf88779253b8adba6823d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2026-06-29 02:54:28 +0530

    smooth animations from ml4w coz i love that!

--- dotfiles/.config/hypr/src/look_and_feel/decorations.lua
+++ dotfiles/.config/hypr/src/look_and_feel/decorations.lua
@@ -1,24 +1,45 @@
 hl.config({
-	decoration = {
-		rounding         = 10,
-		rounding_power   = 2,
+	general = {
+		gaps_in          = 10,
+		gaps_out         = 20,
 
-		-- Change transparency of focused and unfocused windows
-		active_opacity   = 1.0,
-		inactive_opacity = 1.0,
+		border_size      = 2,
 
-		shadow           = {
-			enabled      = true,
-			range        = 4,
-			render_power = 3,
-			color        = 0xee1a1a1a,
+		col              = {
+			active_border   = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			inactive_border = "rgba(595959aa)",
 		},
 
-		blur             = {
-			enabled  = true,
-			size     = 3,
-			passes   = 1,
-			vibrancy = 0.1696,
-		},
+		resize_on_border = true,
+
+		allow_tearing    = false,
 	},
+
+	decoration = {
+		rounding           = 20,
+		rounding_power     = 1,
+
+		active_opacity     = 1.0,
+		inactive_opacity   = 0.8,
+		fullscreen_opacity = 1.0,
+
+		shadow             = {
+			enabled      = true,
+			range        = 32,
+			render_power = 3,
+			color        = "rgba(66000000)",
+		},
+
+		blur               = {
+			enabled           = true,
+			size              = 4,
+			passes            = 1,
+			vibrancy          = 0.1696,
+			new_optimizations = true,
+			ignore_opacity    = true,
+			xray              = false,
+			popups            = false,
+			input_methods     = false,
+		},
+	}
 })
